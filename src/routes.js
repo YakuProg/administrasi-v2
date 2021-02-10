47270a58e132c2375de80b66e714c2def65be4c3
wawandx <rachmadkurniawandev@gmail.com>
2021-02-10 13:24:19 +0800

    chore: add Screens Assets & Users

--- src/routes.js
+++ src/routes.js
@@ -66,6 +66,20 @@ import PayMoney from "./views/CashBank/Transaction/PayMoney";
 // import Logout from './views/Logout';
 // import MenuAssets from './views/assets/menuAssets';
 
+/**
+ * 
+ * WAWAN
+ * 
+ */
+import AddCoopraSale from './views/Sales/AddCoopraSale';
+import AddCoopraContract from './views/Sales/AddCoopraContract';
+import AddGoodsSale from './views/Sales/AddGoodsSale';
+import Expenses from './views/Expenses/Expenses';
+import AddContacts from './views/Contacts/AddContacts';
+import CashierTransaction from './views/CashierTransaction';
+import MenuAssets from './views/Assets/MenuAssets';
+import ManagementUser from './views/ManagementUser';
+
 const dashboardRoutes = [
   {
     path: "/dashboard",
@@ -100,6 +114,62 @@ const dashboardRoutes = [
     name: "Pay Money",
     icon: Person,
     component: PayMoney,
+    layout: "/submenu",
+  },
+  {
+    path: "/add-coopra-sale",
+    name: "Add Coopra Sale",
+    icon: Person,
+    component: AddCoopraSale,
+    layout: "/submenu",
+  },
+  {
+    path: "/add-coopra-contract",
+    name: "Add Coopra Contract",
+    icon: Person,
+    component: AddCoopraContract,
+    layout: "/submenu",
+  },
+  {
+    path: "/add-goods-sale",
+    name: "Add Goods Sale",
+    icon: Person,
+    component: AddGoodsSale,
+    layout: "/submenu",
+  },
+  {
+    path: "/expenses",
+    name: "Expenses",
+    icon: Person,
+    component: Expenses,
+    layout: "/submenu",
+  },
+  {
+    path: "/add-contacts",
+    name: "Add Contacts",
+    icon: Person,
+    component: AddContacts,
+    layout: "/submenu",
+  },
+  {
+    path: "/cashier-transaction",
+    name: "Cashier Transaction",
+    icon: Person,
+    component: CashierTransaction,
+    layout: "/submenu",
+  },
+  {
+    path: "/menu-assets",
+    name: "Assets",
+    icon: Person,
+    component: MenuAssets,
+    layout: "/submenu",
+  },
+  {
+    path: "/management-User",
+    name: "Management User",
+    icon: Person,
+    component: ManagementUser,
     layout: "/submenu",
   },
 ];
